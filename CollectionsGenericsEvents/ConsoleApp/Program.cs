7801fe0aa705c6cb47d76618ef2abd8e45ed2d19
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-11-14 20:38:25 +0200

    added event handling, improved exceptions, added summary, built dll, added console app for demo

---
+++ CollectionsGenericsEvents/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CustomCollection;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>(3);
+
+            stack.StackEvent += ConsoleMessage;
+
+            stack.Push(64);
+            stack.Push(32);
+            stack.Push(128);
+            stack.Peek();
+            stack.Pop();
+            stack.Push(stack.Pop());
+        }
+
+        private static void ConsoleMessage(object source, StackEventArgs args)
+        {
+            Console.WriteLine(args.Message);
+        }
+    }
+}
--- CollectionsGenericsEvents/CustomCollection/MyStack.cs
+++ CollectionsGenericsEvents/CustomCollection/MyStack.cs
@@ -8,22 +8,42 @@ using System.Threading.Tasks;
 namespace CustomCollection
 {
     /// <summary>
-    /// 
+    /// This class is used for throwing empty stack exception message
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    public class EmptyStackException : Exception
+    {
+        public EmptyStackException(string message) : base(message) { }
+    }
 
-    //events
-    //summary description
-    //build solution into dll
-    //create console project
+    /// <summary>
+    /// This class is used for storing arguments in standard event handling
+    /// </summary>
+    public class StackEventArgs : EventArgs
+    {
+        public string Message { get; }
 
+        public StackEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+
+    public delegate void StackHandler(object source, StackEventArgs arg);
+
+    /// <summary>
+    /// This class is used to store data as a custom stack of generic elements.
+    /// The order in which elements come off a stack is LIFO (last in, first out)
+    /// </summary>
+    /// <typeparam name="T">Generic argument</typeparam>
     public class MyStack<T>
     {
         //storage
-        T[] stack = null;
+        T[] stack;
 
         //index of the element on top of the stack
-        int index = -1;
+        int topIndex = -1;
+
+        public event StackHandler StackEvent;
 
         //constructors create stack with certain capacity
         public MyStack(int capacity)
@@ -36,50 +56,56 @@ namespace CustomCollection
             
         }
 
+        //yield operator implements IEnumerator<T> interface in the iterator
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in stack)
+            {
+                yield return item;
+            }
+        }
+
         //pushes an element on the top of the stack
         public void Push(T item) 
         {
-            if (index > stack.Length - 1) 
+            if (topIndex >= stack.Length - 1) 
             {
-                throw new OverflowException();
+                throw new IndexOutOfRangeException("Could not push into full stack");
             }
 
-            stack[index] = item;
+            topIndex++;
 
-            index++;
+            stack[topIndex] = item;
+
+            StackEvent?.Invoke(this, new StackEventArgs($"Push. Now {(topIndex + 1).ToString()} item(s) in stack"));
         }
 
         //removes and returns the top element of the stack
         public T Pop()
         {
-            if (index == -1) 
+            if (topIndex == -1) 
             {
-                throw new Exception();
+                throw new EmptyStackException("Could not pop from empty stack");
             } 
 
-            index--;
+            topIndex--;
 
-            return stack[index];
+            StackEvent?.Invoke(this, new StackEventArgs($"Pop. Now {(topIndex + 1).ToString()} item(s) in stack"));
+
+            return stack[topIndex];
         }
 
         //returns the top element of the stack without removing it
         public T Peek()
         {
-            if (index == -1)
+            if (topIndex == -1)
             {
-                throw new Exception();
+                throw new EmptyStackException("Could not peek into empty stack");
             }
 
-            return stack[index];
-        }
+            StackEvent?.Invoke(this, new StackEventArgs($"Peek. Now {(topIndex + 1).ToString()} item(s) in stack"));
 
-        //yield operator implements IEnumerator<T> interface in the iterator
-        public IEnumerator<T> GetEnumerator()
-        {
-            foreach(var item in stack)
-            {
-                yield return item; 
-            }
+            return stack[topIndex];
         }
     }
 }
